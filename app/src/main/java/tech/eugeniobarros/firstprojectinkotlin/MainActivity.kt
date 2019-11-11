1dbc0d6bc375e7930ec4484a775655f1133217f6
Eugenio Barros <eugenio.barros.jr@gmail.com>
2019-11-11 08:37:16 -0300

    Add: Text changed when button is clicked.

--- app/src/main/java/tech/eugeniobarros/firstprojectinkotlin/MainActivity.kt
+++ app/src/main/java/tech/eugeniobarros/firstprojectinkotlin/MainActivity.kt
@@ -1,12 +1,21 @@
 package tech.eugeniobarros.firstprojectinkotlin
 
-import android.support.v7.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    @SuppressLint("SetTextI18n")
+    fun onClick(view: View) {
+        val text = findViewById<TextView>(R.id.exhibitionTxt)
+        text.text = "The button has Clicked"
     }
 }
